18a9b19d56704b6817ebf73fdb4e453bd5752fab
Đặng Cao Cường <145358486+justccuong@users.noreply.github.com>
2025-09-26 11:36:44 +0700

    Update EX1.1.cpp

--- EX1.1.cpp
+++ EX1.1.cpp
@@ -1,3 +1,7 @@
+// vì B có 10001 phần tử nên ta sẽ để một biến res = 10001, tiếp theo đó chạy 1 vòng lặp và xét các số từ 0 đến 
+// 1 số nào đó mà (gọi số đó là i) i*(i+2) <= 10000 và chia hết cho 15, mỗi khi điều đó xảy ra thì -1 phần tử của
+// tập B.
+
 // ketqua: 9975 
 
 #include <iostream>
