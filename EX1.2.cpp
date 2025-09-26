6ee1c8e91b4669bb2f36902f2087a6d77a445d0c
Đặng Cao Cường <145358486+justccuong@users.noreply.github.com>
2025-09-26 11:40:10 +0700

    Update EX1.2.cpp

--- EX1.2.cpp
+++ EX1.2.cpp
@@ -1,5 +1,10 @@
 //The 100th element of A∩B in the decreasing order is . . . . . . . Note
 //that the greatest element of A ∩ B is 10000.
+
+//phần tử lớn nhất của A giao B là 10000, ta sẽ chạy lùi từ đó.
+//nếu số đó mà thỏa mãn chia hết i*(i+2) mà chia hết cho 15 thì tăng biến đếm
+//cứ lùi i và tăng biến đếm đến khi nó = 100 thì in ra màn hình.
+
 //ketqua: 9627
 
 #include <iostream>
